f0fb1386ea721582a3566282ca89366061b91c40
sporcello7 <79470244+sporcello7@users.noreply.github.com>
2022-05-03 13:35:13 -0400

    update

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ terraform {
 provider "lacework" {}
 
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
 module "aws_config" {
@@ -23,8 +23,9 @@ module "aws_config" {
 }
 
 module "aws_cloudtrail" {
-  source  = "lacework/cloudtrail/aws"
-  version = "~> 2.0"
+  source                = "lacework/cloudtrail/aws"
+  version               = "~> 2.0"
+  use_existing_iam_role = true
   iam_role_name         = module.aws_config.iam_role_name
   iam_role_arn          = module.aws_config.iam_role_arn
   iam_role_external_id  = module.aws_config.external_id
