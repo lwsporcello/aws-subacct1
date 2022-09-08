30212caeae0707c701394cd21c0dc9227a87120c
lwsporcello <79470244+lwsporcello@users.noreply.github.com>
2022-09-08 14:30:26 -0400

    remove agentless integration

--- lacework_agentless.tf
+++ lacework_agentless.tf
@@ -1,7 +1,7 @@
-module "lacework_aws_agentless_scanning_global" {
-  source  = "lacework/agentless-scanning/aws"
-  version = "~> 0.3"
+#module "lacework_aws_agentless_scanning_global" {
+#  source  = "lacework/agentless-scanning/aws"
+#  version = "~> 0.3"
 
-  global                    = true
-  lacework_integration_name = "sidekick_from_terraform"
-}
+#  global                    = true
+#  lacework_integration_name = "TF_agentless_sidekick"
+#}
