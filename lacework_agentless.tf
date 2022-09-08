fee0733b85afe4779050c5e31adb4accdd29e924
lwsporcello <79470244+lwsporcello@users.noreply.github.com>
2022-09-08 16:12:56 -0400

    add agentless integration - remove regional

--- lacework_agentless.tf
+++ lacework_agentless.tf
@@ -5,17 +5,3 @@ module "lacework_aws_agentless_scanning_global" {
   global                    = true
   lacework_integration_name = "TF_agentless_sidekick"
 }
-
-module "lacework_aws_agentless_scanning_regional" {
-  source  = "lacework/agentless-scanning/aws"
-  version = "~> 0.3"
-
-  regional                              = true
-  agentless_scan_ecs_task_role_arn      = module.lacework_aws_agentless_scanning_global.agentless_scan_ecs_task_role_arn
-  agentless_scan_ecs_execution_role_arn = module.lacework_aws_agentless_scanning_global.agentless_scan_ecs_execution_role_arn
-  agentless_scan_ecs_event_role_arn     = module.lacework_aws_agentless_scanning_global.agentless_scan_ecs_event_role_arn
-  agentless_scan_secret_arn             = module.lacework_aws_agentless_scanning_global.agentless_scan_secret_arn
-  lacework_account                      = module.lacework_aws_agentless_scanning_global.lacework_account
-  prefix                                = module.lacework_aws_agentless_scanning_global.prefix
-  suffix                                = module.lacework_aws_agentless_scanning_global.suffix
-}
