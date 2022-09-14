8ef60a58c0309b505e1b1bb8512a4103eb14e387
lwsporcello <79470244+lwsporcello@users.noreply.github.com>
2022-09-14 11:32:47 -0400

    rename ecr

--- ecr.tf
+++ ecr.tf
@@ -1,4 +1,4 @@
 resource "aws_ecr_repository" "sporcello-ecr-subacct" {
-  name                 = "TF-sporcello-ecr-subacct"
+  name                 = "tf-sporcello-ecr-subacct"
   image_tag_mutability = "MUTABLE"
 }
