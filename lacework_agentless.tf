f3bb84324047f3be04e69200aa257f8bdf4b044c
lwsporcello <79470244+lwsporcello@users.noreply.github.com>
2022-11-18 16:17:01 -0500

    update agentless

--- lacework_agentless.tf
+++ lacework_agentless.tf
@@ -2,7 +2,6 @@
 #module "lacework_aws_agentless_scanning_global" {
 #  source  = "lacework/agentless-scanning/aws"
 #  version = "~> 0.3"
-
 #  global                    = true
 #  lacework_integration_name = "TF_agentless_sidekick"
 #}
@@ -11,13 +10,12 @@
 #module "lacework_aws_agentless_scanning_region" {
 #  source  = "lacework/agentless-scanning/aws"
 #  version = ">= 0.3"
-
 #  regional                = true
 #  global_module_reference = module.lacework_aws_agentless_scanning_global
 #}
 
 
-#Org integration - Scanning account
+#Org integration - global resources, scanning resources
 module "lacework_aws_agentless_scanning_global" {
   source  = "lacework/agentless-scanning/aws"
   version = "~> 0.6"
