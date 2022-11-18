3b06b866377c68c5f7ceeba6d85666fd8cbe37d6
lwsporcello <79470244+lwsporcello@users.noreply.github.com>
2022-11-18 12:53:39 -0500

    add agentless global module

--- lacework_agentless.tf
+++ lacework_agentless.tf
@@ -15,3 +15,27 @@
 #  regional                = true
 #  global_module_reference = module.lacework_aws_agentless_scanning_global
 #}
+
+
+#Org integration - Scanning account
+module "lacework_aws_agentless_scanning_global" {
+  source  = "lacework/agentless-scanning/aws"
+  version = "~> 0.6"
+
+  global = true
+  organization = {
+    monitored_accounts = ["975442149240", "626835103576"]
+    management_account = "975442149240"
+  }
+
+  lacework_integration_name = "TF agentless workload org"
+}
+
+module "lacework_aws_agentless_scanning_region" {
+  source  = "lacework/agentless-scanning/aws"
+  version = "~> 0.6"
+
+  regional                = true
+  global_module_reference = module.lacework_aws_agentless_scanning_global
+  vpc_cidr_block          = "10.10.34.0/24"
+}
