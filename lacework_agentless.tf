7fc3f19e092aa4e9fa420fb85ff26f267d85e2ab
lwsporcello <79470244+lwsporcello@users.noreply.github.com>
2022-11-18 11:10:39 -0500

    remove agentless wrk + update lw provider

--- lacework_agentless.tf
+++ lacework_agentless.tf
@@ -1,6 +1,6 @@
 module "lacework_aws_agentless_scanning_global" {
   #removing integration - will be managed via org integration
-  count = 0
+  count   = 0
   source  = "lacework/agentless-scanning/aws"
   version = "~> 0.3"
 
@@ -10,16 +10,17 @@ module "lacework_aws_agentless_scanning_global" {
 
 module "lacework_aws_agentless_scanning_region" {
   #removing integration - will be managed via org integration
-  count = 0
+  count   = 0
   source  = "lacework/agentless-scanning/aws"
   version = ">= 0.3"
 
-  regional                              = true
-  agentless_scan_ecs_task_role_arn      = module.lacework_aws_agentless_scanning_global.agentless_scan_ecs_task_role_arn
-  agentless_scan_ecs_execution_role_arn = module.lacework_aws_agentless_scanning_global.agentless_scan_ecs_execution_role_arn
-  agentless_scan_ecs_event_role_arn     = module.lacework_aws_agentless_scanning_global.agentless_scan_ecs_event_role_arn
-  agentless_scan_secret_arn             = module.lacework_aws_agentless_scanning_global.agentless_scan_secret_arn
-  lacework_account                      = module.lacework_aws_agentless_scanning_global.lacework_account
-  prefix                                = module.lacework_aws_agentless_scanning_global.prefix
-  suffix                                = module.lacework_aws_agentless_scanning_global.suffix
+  regional                = true
+  global_module_reference = module.lacework_aws_agentless_scanning_global
+  #agentless_scan_ecs_task_role_arn      = module.lacework_aws_agentless_scanning_global.agentless_scan_ecs_task_role_arn
+  #agentless_scan_ecs_execution_role_arn = module.lacework_aws_agentless_scanning_global.agentless_scan_ecs_execution_role_arn
+  #agentless_scan_ecs_event_role_arn     = module.lacework_aws_agentless_scanning_global.agentless_scan_ecs_event_role_arn
+  #agentless_scan_secret_arn             = module.lacework_aws_agentless_scanning_global.agentless_scan_secret_arn
+  #lacework_account                      = module.lacework_aws_agentless_scanning_global.lacework_account
+  #prefix                                = module.lacework_aws_agentless_scanning_global.prefix
+  #suffix                                = module.lacework_aws_agentless_scanning_global.suffix
 }
