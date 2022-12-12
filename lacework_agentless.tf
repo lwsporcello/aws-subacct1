855c39b4c1ffa19156bd415839b135e76e0dd8e0
lwsporcello <79470244+lwsporcello@users.noreply.github.com>
2022-12-12 10:07:54 -0500

    removing agentless integration

--- lacework_agentless.tf
+++ lacework_agentless.tf
@@ -15,25 +15,27 @@
 #}
 
 
+#USE BELOW, NOT ABOVE
+
 #Org integration - global resources, scanning resources
-module "lacework_aws_agentless_scanning_global" {
-  source  = "lacework/agentless-scanning/aws"
-  version = "~> 0.6"
+#module "lacework_aws_agentless_scanning_global" {
+#  source  = "lacework/agentless-scanning/aws"
+#  version = "~> 0.6"
 
-  global = true
-  organization = {
-    monitored_accounts = ["975442149240", "626835103576", "153466455358"]
-    management_account = "975442149240"
-  }
+#  global = true
+#  organization = {
+#    monitored_accounts = ["975442149240", "626835103576", "153466455358"]
+#    management_account = "975442149240"
+#  }
 
-  lacework_integration_name = "TF agentless workload org"
-}
+#  lacework_integration_name = "TF agentless workload org"
+#}
 
-module "lacework_aws_agentless_scanning_region" {
-  source  = "lacework/agentless-scanning/aws"
-  version = "~> 0.6"
+#module "lacework_aws_agentless_scanning_region" {
+#  source  = "lacework/agentless-scanning/aws"
+#  version = "~> 0.6"
 
-  regional                = true
-  global_module_reference = module.lacework_aws_agentless_scanning_global
-  vpc_cidr_block          = "10.10.34.0/24"
-}
+#  regional                = true
+#  global_module_reference = module.lacework_aws_agentless_scanning_global
+#  vpc_cidr_block          = "10.10.34.0/24"
+#}
---
+++ lacework_agentless_2.tf
@@ -0,0 +1,15 @@
+#module "lacework_aws_agentless_scanning_global_2" {
+#  source = "github.com/lwsporcello/terraform-aws-agentless-scanning-test"
+  #version = "~> 0.6"
+
+#  global                = true
+#  regional              = true
+#  use_existing_iam_role = true
+
+#  iam_role_arn                          = "my-arn"
+#  agentless_scan_ecs_task_role_arn      = "my-arn"
+#  agentless_scan_ecs_execution_role_arn = "my-arn"
+#  agentless_scan_ecs_event_role_arn     = "my-arn"
+
+#  lacework_integration_name = "TF agentless workload test"
+#}
