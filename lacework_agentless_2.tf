77a108fe57ab7131600b8637e52318df1ba7bc5b
lwsporcello <79470244+lwsporcello@users.noreply.github.com>
2022-12-13 16:58:18 -0500

    add agentless

--- lacework_agentless_2.tf
+++ lacework_agentless_2.tf
@@ -1,6 +1,6 @@
 #module "lacework_aws_agentless_scanning_global_2" {
 #  source = "github.com/lwsporcello/terraform-aws-agentless-scanning-test"
-  #version = "~> 0.6"
+#version = "~> 0.6"
 
 #  global                      = true
 #  regional                    = true
@@ -19,23 +19,50 @@
 #  lacework_integration_name = "TF agentless workload test"
 #}
 
-#module "lacework_aws_agentless_scanning_global_3" {
-#  source = "github.com/lwsporcello/terraform-aws-agentless-scanning"
+module "lacework_aws_agentless_scanning_3" {
+  source = "github.com/lwsporcello/terraform-aws-agentless-scanning"
   #version = "~> 0.6"
 
-#  global                      = true
-#  regional                    = true
+  global   = true
+  regional = true
   #use_existing_iam_role       = true
   #use_existing_event_role     = true
   #use_existing_execution_role = true
   #use_existing_task_role      = true
 
-#  iam_role_arn                          = "arn:aws:iam::626835103576:role/lacework-ca-role"
-#  external_id                           = "VON8ab7cfd0"
-#  agentless_scan_ecs_task_role_arn      = "arn:aws:iam::626835103576:role/lacework-ecs-task-role"
-#  agentless_scan_ecs_execution_role_arn = "arn:aws:iam::626835103576:role/lacework-ecs-execution-role"
-#  agentless_scan_ecs_event_role_arn     = "arn:aws:iam::626835103576:role/lacework-ecs-event-role"
+  #iam_role_arn                          = "arn:aws:iam::626835103576:role/lacework-ca-role"
+  #external_id                           = "VON8ab7cfd0"
+  #agentless_scan_ecs_task_role_arn      = "arn:aws:iam::626835103576:role/lacework-ecs-task-role"
+  #agentless_scan_ecs_execution_role_arn = "arn:aws:iam::626835103576:role/lacework-ecs-execution-role"
+  #agentless_scan_ecs_event_role_arn     = "arn:aws:iam::626835103576:role/lacework-ecs-event-role"
 
-#  suffix                    = "sporcello000x"
+  #suffix                    = "sporcello0002"
+  lacework_integration_name = "TF agentless workload test2"
+}
+
+#single account multi region example
+#provider "aws" {
+#  alias = "usw2"
+#  region = "us-west-2"
+#}
+#module "lacework_aws_agentless_scanning_global" {
+#  source = "github.com/lwsporcello/terraform-aws-agentless-scanning"
+#  global                    = true
 #  lacework_integration_name = "TF agentless workload test2"
 #}
+#module "lacework_aws_agentless_scanning_regional_1" {
+#  source = "github.com/lwsporcello/terraform-aws-agentless-scanning"
+#  regional                  = true
+#  global_module_reference   = module.lacework_aws_agentless_scanning_global
+#  lacework_integration_name = "TF agentless workload test2"
+#}
+#module "lacework_aws_agentless_scanning_regional_2" {
+#  source = "github.com/lwsporcello/terraform-aws-agentless-scanning"
+#  providers = {
+#    aws = aws.usw2
+#  }
+#  regional                  = true
+#  global_module_reference   = module.lacework_aws_agentless_scanning_global
+#  lacework_integration_name = "TF agentless workload test2"
+#}
+
