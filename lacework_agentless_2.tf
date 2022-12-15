896a6a8a42d236af6bc9accf80167465305cd32c
lwsporcello <79470244+lwsporcello@users.noreply.github.com>
2022-12-15 10:26:24 -0500

    levelset

--- lacework_agentless_2.tf
+++ lacework_agentless_2.tf
@@ -1,16 +1,16 @@
 #module "lacework_aws_agentless_scanning_global_2" {
-#  source = "github.com/lwsporcello/terraform-aws-agentless-scanning-test"
+#  source = "github.com/lwsporcello/terraform-aws-agentless-scanning"
 #version = "~> 0.6"
 
-#  global                      = true
-#  regional                    = true
-#  use_existing_iam_role       = true
-#  use_existing_event_role     = true
-#  use_existing_execution_role = true
-#  use_existing_task_role      = true
+#  global                          = true
+#  regional                        = true
+#  use_existing_cross_account_role = true
+#  use_existing_event_role         = true
+#  use_existing_execution_role     = true
+#  use_existing_task_role          = true
 
-#  iam_role_arn                          = "arn:aws:iam::626835103576:role/lacework-ca-role"
-#  iam_role_external_id                  = "VON8ab7cfd0"
+#  cross_account_role_arn                = "arn:aws:iam::626835103576:role/lacework-ca-role"
+#  external_id                           = "VON8ab7cfd0"
 #  agentless_scan_ecs_task_role_arn      = "arn:aws:iam::626835103576:role/lacework-ecs-task-role"
 #  agentless_scan_ecs_execution_role_arn = "arn:aws:iam::626835103576:role/lacework-ecs-execution-role"
 #  agentless_scan_ecs_event_role_arn     = "arn:aws:iam::626835103576:role/lacework-ecs-event-role"
