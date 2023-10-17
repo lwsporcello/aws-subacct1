209d028f29e5cbc492abd3fc7496b77e339072f9
lwsporcello <79470244+lwsporcello@users.noreply.github.com>
2023-10-17 13:32:37 -0400

    rm lw int

--- lacework-agentless.tf
+++ lacework-agentless.tf
@@ -1,14 +1,14 @@
 #single account multi region example
-provider "aws" {
-  alias  = "usw2"
-  region = "us-west-2"
-}
+#provider "aws" {
+#  alias  = "usw2"
+#  region = "us-west-2"
+#}
 
-module "lacework_aws_agentless_scanning_global" {
-  source  = "lacework/agentless-scanning/aws"
-  version = "~> 0.5"
+#module "lacework_aws_agentless_scanning_global" {
+#  source  = "lacework/agentless-scanning/aws"
+#  version = "~> 0.5"
 
-  global                    = true
-  regional                  = true
-  lacework_integration_name = "TF-agentless-sub1"
-}
+#  global                    = true
+#  regional                  = true
+#  lacework_integration_name = "TF-agentless-sub1"
+#}
