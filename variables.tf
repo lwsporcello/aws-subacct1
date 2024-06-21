589f8dc7b2842641cae19c15758e0817543572fc
lwsporcello <79470244+lwsporcello@users.noreply.github.com>
2024-06-21 13:31:09 -0400

    fix

--- variables.tf
+++ variables.tf
@@ -3,14 +3,6 @@ variable "AWS_REGION" {
   type = string
 }
 
-variable "AWS_ACCESS_KEY_ID" {
-  type = string
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  type = string
-}
-
 #lacework
 variable "lacework_account_name" {
   type = string
