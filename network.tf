b18ab797a6438f9490fa105587c177d1cee5a398
lwsporcello <79470244+lwsporcello@users.noreply.github.com>
2024-06-21 14:07:43 -0400

    build network

---
+++ network.tf
@@ -0,0 +1,89 @@
+#VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.10.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main"
+    env  = "default"
+  }
+}
+
+#Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+    env  = "default"
+  }
+}
+
+#Route Tables
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "main"
+    env  = "default"
+  }
+}
+
+resource "aws_main_route_table_association" "main-outbound" {
+  vpc_id         = aws_vpc.main.id
+  route_table_id = aws_route_table.main.id
+}
+
+#Subnets
+resource "aws_subnet" "public-subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.10.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_subnet" "private-subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.10.2.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+#Security Groups
+resource "aws_security_group" "db-sg" {
+  name        = "db-sg"
+  description = "Allow incoming traffic to the Linux EC2 Instance"
+  vpc_id      = aws_vpc.main.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow incoming SSH connections from public internet"
+  }
+  ingress {
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = ["10.10.0.0/16"]
+    description = "Allow incoming db connections only from VPC"
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
