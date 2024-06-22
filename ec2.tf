ad87dd540b7af0e223b2358ef04a68f0a7f4ab2b
lwsporcello <79470244+lwsporcello@users.noreply.github.com>
2024-06-22 10:02:36 -0400

    replace ec2

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,20 @@
 resource "aws_key_pair" "ssh_keypair" {
-  key_name = "mongodb-keypair"
+  key_name   = "mongodb-keypair"
   public_key = file("./mongo.pub")
+}
+
+resource "aws_instance" "my_instance" {
+  count                       = 1
+  ami                         = "ami-0fc5d935ebf8bc3bc" # Ubuntu 20.04 LTS
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.ssh_keypair.key_name
+  subnet_id                   = aws_subnet.public-subnet.id
+  security_groups             = [aws_security_group.db-sg.id]
+  associate_public_ip_address = true
+  user_data                   = filebase64("./build_mongodb.sh")
+  iam_instance_profile        = aws_iam_instance_profile.mongodb_profile.name
+  tags = {
+    Name = "mongodb server"
+
+  }
 }
---
+++ iam.tf
@@ -0,0 +1,34 @@
+data "aws_iam_policy_document" "ec2_instance_assume_role_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com", ]
+    }
+  }
+
+}
+
+data "aws_iam_policy_document" "iam_policy_ec2_instance_document" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:*"]
+    resources = ["*"]
+  }
+
+}
+
+resource "aws_iam_role" "mongodb_instance_role" {
+  name               = "ec2_instance_role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_instance_assume_role_policy.json
+  inline_policy {
+    name   = "mongodb-instance-role"
+    policy = data.aws_iam_policy_document.iam_policy_ec2_instance_document.json
+  }
+}
+
+resource "aws_iam_instance_profile" "mongodb_profile" {
+  name = "mongodb-instance-profile"
+  role = aws_iam_role.mongodb_instance_role.name
+}
