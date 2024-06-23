b5bbb26eb48b309ad81aff22f89e9b9a4a97db98
lwsporcello <79470244+lwsporcello@users.noreply.github.com>
2024-06-23 10:13:27 -0400

    adjust bucket acls

--- s3.tf
+++ s3.tf
@@ -5,3 +5,29 @@ resource "aws_s3_bucket" "mongodb-backup" {
   }
   force_destroy = true
 }
+
+resource "aws_s3_bucket_ownership_controls" "mongodb-backup" {
+  bucket = aws_s3_bucket.mongodb-backup.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "mongodb-backup" {
+  bucket = aws_s3_bucket.mongodb-backup.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "mongodb-backup" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.mongodb-backup,
+    aws_s3_bucket_public_access_block.mongodb-backup,
+  ]
+
+  bucket = aws_s3_bucket.mongodb-backup.id
+  acl    = "public-read"
+}
