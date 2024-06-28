ba5965a9669c8f3d8ab0a1df666355fc7c4e7dee
lwsporcello <79470244+lwsporcello@users.noreply.github.com>
2024-06-28 15:32:47 -0400

    add bucket policy

--- s3.tf
+++ s3.tf
@@ -31,3 +31,16 @@ resource "aws_s3_bucket_acl" "mongodb-backup" {
   bucket = aws_s3_bucket.mongodb-backup.id
   acl    = "public-read"
 }
+
+data "aws_iam_policy_document" "s3_read_permissions" {
+  statement {
+    effect = "Allow"
+    actions = ["s3:*"]
+    resources = [aws_s3_bucket.mongodb-backup.id]
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.mongodb-backup.id
+  policy = data.aws_iam_policy_document.s3_read_permissions
+}
