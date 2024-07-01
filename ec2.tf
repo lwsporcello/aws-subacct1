a213cd8aeccdbc133dc57b4d8519afea21ab3452
lwsporcello <79470244+lwsporcello@users.noreply.github.com>
2024-07-01 11:08:51 -0400

    rm ec2

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,11 @@
 resource "aws_key_pair" "ssh_keypair" {
-  count      = 1
+  count      = 0
   key_name   = "mongodb-keypair"
   public_key = file("./mongo.pub")
 }
 
 resource "aws_instance" "mongodb_instance" {
-  count                       = 1
+  count                       = 0
   ami                         = "ami-0fc5d935ebf8bc3bc" # Ubuntu 20.04 LTS
   instance_type               = "t2.micro"
   key_name                    = aws_key_pair.ssh_keypair[0].key_name
